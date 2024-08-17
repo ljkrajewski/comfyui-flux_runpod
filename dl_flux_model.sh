d6eecea3fa0f5d4a1109cb2afccecd08d1d8ab12
Leo Krajewski <leo.krajewski@aol.com>
2024-08-17 08:45:25 -0400

    Changed model back to v1 from v2

--- dl_flux_model.sh
+++ dl_flux_model.sh
@@ -9,7 +9,7 @@ HF_TOKEN="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"  #It's read-only. Don't get exc
 #cd $DEST_DIR
 #wget -c https://huggingface.co/Kijai/flux-fp8/resolve/main/flux1-dev-fp8.safetensors -P ./models/checkpoints/
 #wget -c https://huggingface.co/Comfy-Org/flux1-dev/resolve/main/flux1-dev-fp8.safetensors --header="Authorization: Bearer $HF_TOKEN" -P ./models/checkpoints/
-wget -c https://huggingface.co/lllyasviel/flux1-dev-bnb-nf4/resolve/main/flux1-dev-bnb-nf4-v2.safetensors --header="Authorization: Bearer $HF_TOKEN" -P ./models/checkpoints/
+wget -c https://huggingface.co/lllyasviel/flux1-dev-bnb-nf4/resolve/main/flux1-dev-bnb-nf4.safetensors --header="Authorization: Bearer $HF_TOKEN" -P ./models/checkpoints/
 #wget -c https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors -P ./models/clip/
 #wget -c https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors -P ./models/clip/
 #wget -c https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors -P ./models/clip/
