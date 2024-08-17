18fc064488fb78472eabcfcf96f33a730004c747
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2024-08-16 21:00:07 -0400

    Update dl_flux_model.sh

--- dl_flux_model.sh
+++ dl_flux_model.sh
@@ -3,14 +3,14 @@ DEST_DIR=./models
 HF_TOKEN="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"  #It's read-only. Don't get excited.
 
 [ ! -d "./models/checkpoints" ] && mkdir -p ./models/checkpoints
-[ ! -d "./models/clip" ] && mkdir -p ./models/clip
-[ ! -d "./models/vae" ] && mkdir -p ./models/vae
+#[ ! -d "./models/clip" ] && mkdir -p ./models/clip
+#[ ! -d "./models/vae" ] && mkdir -p ./models/vae
 
 #cd $DEST_DIR
 #wget -c https://huggingface.co/Kijai/flux-fp8/resolve/main/flux1-dev-fp8.safetensors -P ./models/checkpoints/
 #wget -c https://huggingface.co/Comfy-Org/flux1-dev/resolve/main/flux1-dev-fp8.safetensors --header="Authorization: Bearer $HF_TOKEN" -P ./models/checkpoints/
 wget -c https://huggingface.co/lllyasviel/flux1-dev-bnb-nf4/resolve/main/flux1-dev-bnb-nf4-v2.safetensors --header="Authorization: Bearer $HF_TOKEN" -P ./models/checkpoints/
-wget -c https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors -P ./models/clip/
+#wget -c https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors -P ./models/clip/
 #wget -c https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors -P ./models/clip/
-wget -c https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors -P ./models/clip/
-wget -c https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors --header="Authorization: Bearer $HF_TOKEN" -P ./models/vae
+#wget -c https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors -P ./models/clip/
+#wget -c https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors --header="Authorization: Bearer $HF_TOKEN" -P ./models/vae
